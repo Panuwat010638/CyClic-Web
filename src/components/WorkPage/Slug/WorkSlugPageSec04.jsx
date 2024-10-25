0e16e7798a4cbc6b40ed1531b801e427f80679c1
Panuwat010638 <132049125+develop-cyclick@users.noreply.github.com>
2024-10-25 14:37:29 +0700

    fix

--- src/components/WorkPage/Slug/WorkSlugPageSec04.jsx
+++ src/components/WorkPage/Slug/WorkSlugPageSec04.jsx
@@ -44,7 +44,7 @@ export default function WorkSlugPageSec04({data}) {
                         {item?.title}
                       </h3>
                       {/* Body */}
-                      <p className={`text-[18px] text-[#161616] font-normal leading-[150%] ${ibm.className}`}>
+                      <p className={`text-[18px] text-[#161616] font-normal leading-[150%] whitespace-pre-line ${ibm.className}`}>
                         {item?.detail}
                       </p>
                     </div>
